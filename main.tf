ef39a363743e83ff7311f01ac4086247db52b856
deep-kaizen <suryaa.e.suryaa.e@accenture.com>
2026-01-29 12:37:40 +0530

    First commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "site" {
+  bucket = var.bucket_name
+}
+
+# Enable static website hosting
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+# Disable block public access (required)
+
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Public read bucket policy
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.site.arn}/*"
+    }]
+  })
+}
+
+# Upload index.html
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.site.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.site.website_endpoint
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "bucket_name" {
+  default = "capstone-suryaa-project-2"
+  description = "Globally unique bucket name"
+  type        = string
+}
